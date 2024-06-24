2012ca3e2f1adc0f39b838902543c246b2b82306
Abhishek Pandey <abhishekpandey7676@gmail.com>
2024-06-24 15:34:32 +0530

    changes

---
+++ first.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class first{
+    public static void main(String args[]){
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+
+        //variables
+        // int a = 25;
+        // int b = 25;
+        // int sum = a+b;
+        // System.out.println(sum);
+
+        //inputs
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        //print sum of two numbers
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first Number:");
+        int a = sc.nextInt();
+        System.out.print("\nEnter Second Number:");
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+
+    }
+}
